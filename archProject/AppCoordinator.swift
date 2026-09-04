849d2ce6f4a0c044433c63899d764fd6c9a05357
Ana Julia <anamolinos52@gmail.com>
2026-09-04 17:04:58 -0300

    scene delegate initial setup

---
+++ archProject/AppCoordinator.swift
@@ -0,0 +1,21 @@
+//
+//  AppCoordinator.swift
+//  archProject
+//
+//  Created by Ana Julia Molinos Leite Da Silva on 04/09/26.
+//
+
+import Foundation
+import UIKit
+
+class AppCoordinator {
+    
+    // MARK: - Properties
+    
+    
+    
+    func start() -> UINavigationController {
+        
+        return UINavigationController()
+    }
+}
---
+++ archProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  archProject
+//
+//  Created by Ana Julia Molinos Leite Da Silva on 04/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
